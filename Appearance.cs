99c223ac3697112d732661e0a2e01d66174f71f0
Faolan Big <faolan.big@web.de>
2024-09-17 15:35:52 +0200

    update

--- Appearance.cs
+++ Appearance.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿/*
+    bigbyte serves as a package manager.
+    It serves as a management tool for downloading, installing, updating, upgrading, removing and running various software aplications from the command line.
+
+    The latest source code can be found on https://github.com/FaolanBig/bigbyte
+
+    Copyright (C) 2024  Carl Öttinger (Carl Oettinger)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+    You can contact me in the following ways:
+        EMail: oettinger.carl@web.de, big-programming@web.de
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
--- Helper.cs
+++ Helper.cs
@@ -17,7 +17,7 @@
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
 
     You can contact me in the following ways:
         EMail: oettinger.carl@web.de, big-programming@web.de
