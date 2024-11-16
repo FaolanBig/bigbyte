e13bb0e4a42d89884606ed461a0d824694990488
Faolan Big <faolan.big@web.de>
2024-11-16 16:15:00 +0100

    update

---
+++ ConfigHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bigbyte
+{
+    internal static class ConfigHandler
+    {
+        internal static void loadConfig_Main()
+        {
+            if (!File.Exists(VarHold.configMain))
+            {
+                ToLog.Wrn($"no config file (main) found at {VarHold.configMain} under {VarHold.configDirectory}");
+                ConfigHandler_init.initConfiguration_default();
+            }
+        }
+    }
+
+    internal static class ConfigHandler_init
+    {
+        internal static void initConfiguration_default()
+        {
+            ToLog.Inf("initializing default config (main)");
+
+            ConfigStruct defaultConfig = new ConfigStruct()
+            {
+                ////////////////////
+            };
+        }
+    }
+
+    public class ConfigStruct
+    {
+        public string IndexDir { get; set; }
+        public string installPath_programs { get; set; }
+        public string tempDirectory { get; set; }
+        public string helpFilePath { get; set; }
+    }
+}
